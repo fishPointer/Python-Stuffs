5f89eb05b4b1fed311c922cb305c87b2ae815a3c
fishPointer <fishPointer@protonmail.com>
2021-05-18 10:00:55 -0700

    Tkinter for now

--- Tkinters/tk-tester.py
+++ Tkinters/tk-tester.py
@@ -4,8 +4,22 @@ from tkinter import *
 
 root = Tk()
 
-myLabel = Label(root, text="Hello world")
-myLabel.pack()
+#This defines the label
+myLabel1 = Label(root, text="Hello world")
+myLabel2 = Label(root, text="This is my pretty boy swag")
+#can be done this way
+# myLabel2 = Label(root, text="This is my pretty boy swag").grid(row=0, column=0)
 
+myButton = Button(root, text="Click Me!")
+
+
+
+
+
+#This Renders the Labels
+myLabel1.grid(row=0, column=0)
+myLabel2.grid(row=1, column=0)
+myButton.grid(row=1, column=1)
+
+#The window close X button interrupts this loop
 root.mainloop()
-
