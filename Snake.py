83a0dc0a63d65f95f630bcc234a70313ca70b878
fishPointer <fishPointer@protonmail.com>
2021-05-18 06:07:30 -0700

    Starting up Snake, and venv 2

---
+++ Snake.py
@@ -0,0 +1,50 @@
+import pygame
+import math
+import random
+import tkinter as tk 
+from tkinter import messagebox
+
+#Using Objects and Classes in this one
+
+class cube(object):
+    rows = 0
+    w = 0
+    def __init__(self, start, dirnx=1, dirny=0, color=(2550,0,0)):
+        pass
+
+    def move(self, dirnx, dirny):
+        pass
+
+    def draw(self, surface, eyes=False):
+        pass
+
+class snake(object):
+    def __init__(self, color, pos):
+        pass
+
+    def move(self):
+        pass
+
+    def reset(self, pos):
+        pass
+
+    def addCube(self):
+        pass
+
+    def draw(self, surface):
+        pass
+
+def drawGrid(w, rows, surface):
+    pass
+
+def redrawWindow(surface):
+    pass
+
+def randomSnack(rows, items):
+    pass
+
+def message_box(subject, content):
+    pass
+
+def main():
+    pass
