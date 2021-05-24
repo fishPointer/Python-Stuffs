8dd77ac74bee4ba65f19e58d2f1df843460af2a6
fishPointer <fishPointer@protonmail.com>
2021-05-24 08:34:37 -0700

    file dialog

---
+++ Tkinters/tk-tester11.py
@@ -0,0 +1,22 @@
+from tkinter import *
+from PIL import ImageTk,Image
+from tkinter import messagebox
+from tkinter import filedialog
+
+root = Tk()
+root.title('Breadbox GUI')
+img = PhotoImage(file='/home/eein/Documents/Lab/Code/Python Stuffs/Tkinters/fsl.png')
+root.call('wm', 'iconphoto', root._w, img)
+
+
+def open():
+    global imgg
+    root.filename = filedialog.askopenfilename(initialdir="/home/eein/Pictures/Anime Girls", title="Select a girl", filetypes=(("png files", "*.png"), ("all files", "*.*")))
+    lbl = Label(root, text=root.filename).pack()
+    imgg = ImageTk.PhotoImage(Image.open(root.filename))
+    imgglbl = Label(image=imgg).pack()
+
+
+btn = Button(root, text="Open a girl", command=open).pack()
+
+root.mainloop()
