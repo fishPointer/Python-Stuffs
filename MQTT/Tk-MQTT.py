28f02aa9c2463b9a725b5028f668b51d75cb32f0
fishPointer <fishPointer@protonmail.com>
2021-05-25 08:53:02 -0700

    Added RGB Sliders

--- MQTT/Tk-MQTT.py
+++ MQTT/Tk-MQTT.py
@@ -24,6 +24,13 @@ def megaon():
 def phoneping():
     client.publish("/phone/ping", "1")
     
+def submitcolors():
+    r = horizontal.get()
+    g = horizontal2.get()
+    b = horizontal3.get()
+    client.publish("/corn/color/r", r)
+    client.publish("/corn/color/g", g)
+    client.publish("/corn/color/b", b)
 
 client = mqtt.Client()
 client.on_connect = on_connect
@@ -32,7 +39,7 @@ client.connect("192.168.0.193", 1883, 60)
 
 root = Tk()
 root.title("Breadnet Wire")
-root.geometry("400x200")
+root.geometry("400x400")
 
 btn = Button(root, text="Connect to MQTT", command=client.loop_start).pack()
 btn2 = Button(root, text="MegaLights OFF", command=megaoff).pack()
@@ -42,5 +49,22 @@ btn5 = Button(root, text="Ping Phone", command=phoneping).pack()
 
 lbl2 = Label(root, font=('calibri', 16, 'bold'))
 lbl2.pack(anchor='center')
+
+
+lbl_r = Label(root, text="Red").pack()
+horizontal = Scale(root, from_=0, to=255, orient=HORIZONTAL)
+horizontal.pack()
+
+lbl_g = Label(root, text="Green").pack()
+horizontal2 = Scale(root, from_=0, to=255, orient=HORIZONTAL)
+horizontal2.pack()
+
+lbl_b = Label(root, text="Blue").pack()
+horizontal3 = Scale(root, from_=0, to=255, orient=HORIZONTAL)
+horizontal3.pack()
+
+btn_rgb = Button(root, text="Submit Colors", command=submitcolors).pack()
+
+
 
 root.mainloop()
